510f6fe310b530d08e919b04717ca222afded93b
Zahusek <zahusek@gmail.com>
2015-06-28 20:25:42 +0200

    Update TestCommand.java

--- src/com/gmail/zahusek/TestCommand.java
+++ src/com/gmail/zahusek/TestCommand.java
@@ -8,7 +8,7 @@ public class TestCommand implements CommandListener {
 	
 	@Cmd(name = "test1", aliases = {"test"}, permission = "test.hh")
 	public void command(CommandSender cs, String[] args){
-		cs.sendMessage("");
+		cs.sendMessage("Look !");
 	}
 	@Sub(parent = "test1", args = {"test", "", "message"}, permission = "test.message")
 	public void subcommand(CommandSender cs, String[] args){
